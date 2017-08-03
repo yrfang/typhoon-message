dab7cfe7404a247980253cce6345b10877fb26a3
yrfang <fyr1116@gmail.com>
2017-08-03 14:02:55 +0800

    pagination button style

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
 .Pagination
-  .test Hi
+  select.form-control(v-model="pageCount")
+    option(v-for="length in pageLength",
+           v-bind:value="length.value") {{length.text}}
+  h5 {{ pageCount }}
+  br
+  nav.paginationBar
+    ul
+      li(v-for="page in totalPage")
+        .button {{ page }}
 </template>
 
 <script>
@@ -9,11 +17,44 @@ export default {
   props: ['dataFilterByArea'],
   data() {
     return {
-
+      pageLength: [
+        { text: "20 筆/頁", value: 20},
+        { text: "30 筆/頁", value: 30},
+        { text: "50 筆/頁", value: 50},
+        { text: "100 筆/頁", value: 100},
+      ],
+      pageCount: 20,
+      pagination: {
+        range: 5,
+        currentPage: 1,
+        filterData: [],
+        data: [],
+      },
     }
   },
+  computed: {
+    totalPage() {
+      return Math.ceil(this.dataFilterByArea.length/this.pageCount);
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="sass">
+.paginationBar
+  ul
+    margin: 0px
+    padding: 0px
+    li
+      list-style: none
+      display: inline-block
+      border: solid 1px #777
+      text-align: center
+      cursor: pointer
+      .button
+        width: 45px
+        height: initial
+        padding: 8px
+        margin-left: auto
+        margin-right: auto
 </style>
