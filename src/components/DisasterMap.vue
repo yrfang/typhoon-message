d75ad007bb7744e451ef6e59e0c0945ca1e88342
yrfang <fyr1116@gmail.com>
2017-08-07 14:24:44 +0800

    getData in map component

--- src/components/DisasterMap.vue
+++ src/components/DisasterMap.vue
@@ -1,11 +1,35 @@
 <template lang="pug">
 .DisasterMap
   h4 Hi, google map coming soon...
+  #map
+//- <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+const DisasterApiUrl = "https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json";
+
+export default {
+  data() {
+    return {
+      disasterData: [],
+    }
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      axios.get(DisasterApiUrl).then((response) => {
+        this.disasterData = response.data.DataSet['diffgr:diffgram'].NewDataSet.CASE_SUMMARY;
+        console.log(this.disasterData);
+      }).catch((error) => { console.log(error); });
+    },
+    mapReady() {
+      
+    },
+  },
 }
 </script>
 
