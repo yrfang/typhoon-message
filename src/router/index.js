3e3e8051f9f766fa550c2c2189ad02bf536990bb
yrfang <fyr1116@gmail.com>
2017-08-07 11:55:12 +0800

    link the fontawesome cdn

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+// import 'font-awesome/css/font-awesome.css';
+
 Vue.use(Router);
 Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
