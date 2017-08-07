908977e9f8a39d110a3faa1ac1ba5dbd1940219a
yrfang <fyr1116@gmail.com>
2017-08-07 11:04:52 +0800

    🔥

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,6 +60,22 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass!resolve-url!sass?sourceMap'
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
       }
     ]
   }
